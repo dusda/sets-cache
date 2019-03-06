a052f20cab19578647bef9fc4d680061cebca700
Dustin Dahl <dustin.dahl@gmail.com>
2019-03-05 17:52:47 -0700

    omg there we go.

--- src/publish.sh
+++ src/publish.sh
@@ -1,3 +1,3 @@
 dotnet pack -c Release;
-dotnet nuget push SetsCache/bin/Release/SetsCache.*.nupkg --api-key $NUGET_API_KEY --source https://api.nuget.org/v3/index.json;
-dotnet nuget push SetsCache.Redis/bin/Release/SetsCache.Redis.*.nupkg --api-key $NUGET_API_KEY --source https://api.nuget.org/v3/index.json;
+dotnet nuget push SetsCache/bin/Release/sets-cache.*.nupkg --api-key $NUGET_API_KEY --source https://api.nuget.org/v3/index.json;
+dotnet nuget push SetsCache.Redis/bin/Release/sets-cache-redis.*.nupkg --api-key $NUGET_API_KEY --source https://api.nuget.org/v3/index.json;
