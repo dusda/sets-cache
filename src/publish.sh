a49486bbbb904a3ace8a0acb057550ba68ea4aec
Dustin Dahl <dustin.dahl@gmail.com>
2019-03-05 17:43:43 -0700

    okay, this should work.

--- src/publish.sh
+++ src/publish.sh
@@ -1,4 +1,3 @@
-  cd src;
-  dotnet pack -c Release;
-  dotnet nuget push SetsCache/bin/Release/SetsCache.*.nupkg --api-key $NUGET_API_KEY;
-  dotnet nuget push SetsCache.Redis/bin/Release/SetsCache.Redis.*.nupkg --api-key $NUGET_API_KEY;
+dotnet pack -c Release;
+dotnet nuget push SetsCache/bin/Release/SetsCache.*.nupkg --api-key $NUGET_API_KEY -Source https://api.nuget.org/v3/index.json;
+dotnet nuget push SetsCache.Redis/bin/Release/SetsCache.Redis.*.nupkg --api-key $NUGET_API_KEY -Source https://api.nuget.org/v3/index.json;
